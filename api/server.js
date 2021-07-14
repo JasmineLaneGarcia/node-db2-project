334d462d7d1e03e2a8f10a909653d4db9046dbe9
Jasmine Lane-Garica <jasminerlanegarcia@gmail.com>
2021-07-14 00:00:08 -0700

    Almost done

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+
 const carsRouter = require('./cars/cars-router')
 
 const server = express()
@@ -7,9 +8,16 @@ server.use(express.json())
 
 server.use("/api/cars", carsRouter)
 
-server.use("*", (req, res) => {
-    res.status(400).json({
+server.use("*", (req, res, next) => {
+    next({ 
+        status: 404,
         message: "Sorry, not found!",
+    })
+})
+
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message,
     })
 })
 
